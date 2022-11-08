85869d93e98b4151904f21251917e133f146a8a5
KSY173 <112866329+KSY173@users.noreply.github.com>
2022-11-08 16:52:59 +0900

    practice3 수정

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[30] = "happy C programming";
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
 	
-	int i = strlen(str);
+	strcpy (dst, src);
 	
-	printf("문자열\"%s\"의 길이 : %i", str, i);
+	printf("copied string : %s", dst);
 	
 	return 0;
 }
